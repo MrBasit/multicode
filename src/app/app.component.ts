8a5e4dd6ca0e1a7d9a8e5c238f7ebb4378bfb31b
BLJC/BasitA <abdul.basit@bgis.com>
2021-12-13 13:28:06 -0500

    initial setup for code editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import 'src/editor/lib/codemirror.js';
+declare const mytest: any;
+declare const CodeMirror: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Multicode';
+  constructor() {}
+  ngOnInit(): void {
+    var mcm = CodeMirror(document.getElementById('Editor'), {
+      lineNumbers: true,
+      mode: 'javascript',
+    });
+    console.log(mcm.getValue());
+    mytest();
+  }
 }
