a397a6e01312109b9188a32d30d2947f373d4f04
Munawar080 <munawarubit956@gmail.com>
2022-02-24 01:10:22 +0500

    add file input

--- src/app/sidebarcomponent/sidebarcomponent.component.ts
+++ src/app/sidebarcomponent/sidebarcomponent.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'sidebarcomponent',
@@ -6,7 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebarcomponent.component.css'],
 })
 export class SidebarcomponentComponent implements OnInit {
+  @HostListener('window:keydown.enter', ['$event']) getInputFieldValue(
+    event: any
+  ) {
+    console.log(event.target.value);
+    event.target.value = '';
+    this.show = false;
+  }
+
+  show = false;
+
+  // form
+  files = new FormGroup({
+    newFile: new FormControl(),
+  });
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  showHide() {
+    this.show = !this.show;
+  }
 }
