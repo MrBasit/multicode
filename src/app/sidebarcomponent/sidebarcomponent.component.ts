cb27847b5cc81c7aeafa6d713be1cf7b1face86d
Munawar080 <munawarubit956@gmail.com>
2022-02-24 16:36:20 +0500

    change position new file control

--- src/app/sidebarcomponent/sidebarcomponent.component.ts
+++ src/app/sidebarcomponent/sidebarcomponent.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, HostListener, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { EventManager } from '@angular/platform-browser';
 
 @Component({
   selector: 'sidebarcomponent',
@@ -22,11 +23,19 @@ export class SidebarcomponentComponent implements OnInit {
     newFile: new FormControl(),
   });
 
+  currentFiles = ['index.js', 'index.txt'];
+
   constructor() {}
 
   ngOnInit(): void {}
 
   showHide() {
     this.show = !this.show;
+  }
+  getFileValue(event: any) {
+    console.log(event.target.value);
+    // for (let index = 0; index < this.currentFiles.length; index++) {
+    //   console.log(this.currentFiles[index]);
+    // }
   }
 }
